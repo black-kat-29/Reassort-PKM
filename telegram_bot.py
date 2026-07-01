664319a0e5bd501fec6adbcc9fd9ca91cbfbb04d
black-kat-29 <black.kat.tcg@gmail.com>
2026-07-01 21:43:24 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,4 @@
+from product_manager import add_product
 from product_manager import load_products
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
@@ -45,3 +46,20 @@ def build_app():
     app.add_handler(CommandHandler("list", list_products))
 
     return app
+
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
+    if len(context.args) == 0:
+        await update.message.reply_text(
+            "Utilisation :\n/add <lien>"
+        )
+        return
+
+    url = context.args[0]
+
+    success, message = add_product(url)
+
+    if success:
+        await update.message.reply_text("✅ " + message)
+    else:
+        await update.message.reply_text("❌ " + message)
