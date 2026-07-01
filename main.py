3b40265d25ef5894248bf75c0fc4ad1bfd8c5b4e
black-kat-29 <black.kat.tcg@gmail.com>
2026-07-01 22:58:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from scheduler import start_scheduler
 from telegram_bot import build_app
 
 def main():
+    start_scheduler()
+
     app = build_app()
-    print("✅ StockPKM est lancé")
+
+    print("🚀 StockPKM est lancé")
+
     app.run_polling()
 
 if __name__ == "__main__":
