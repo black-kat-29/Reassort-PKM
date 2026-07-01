a82e93f00627b85d1095a4da164cd9636a457045
black-kat-29 <black.kat.tcg@gmail.com>
2026-07-01 21:40:51 +0200

    Update product_manager.py

--- product_manager.py
+++ product_manager.py
@@ -13,3 +13,43 @@ def load_products():
 def save_products(products):
     with open(PRODUCTS_FILE, "w", encoding="utf-8") as f:
         json.dump(products, f, indent=2, ensure_ascii=False)
+
+import re
+
+def add_product(url):
+    products = load_products()
+
+    if "amazon" in url:
+        match = re.search(r"/dp/([A-Z0-9]{10})", url)
+
+        if not match:
+            return False, "ASIN introuvable."
+
+        asin = match.group(1)
+
+        for product in products:
+            if product.get("asin") == asin:
+                return False, "Produit déjà présent."
+
+        products.append({
+            "site": "amazon",
+            "asin": asin
+        })
+
+    elif "fnac" in url:
+
+        for product in products:
+            if product.get("url") == url:
+                return False, "Produit déjà présent."
+
+        products.append({
+            "site": "fnac",
+            "url": url
+        })
+
+    else:
+        return False, "Site non pris en charge."
+
+    save_products(products)
+
+    return True, "Produit ajouté."
